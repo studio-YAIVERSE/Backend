e3dd35cbeed18bed8c56aa41a43d9430cf849ea4
kdha0727 <dhakim@yonsei.ac.kr>
2023-04-13 20:12:43 +0900

    fix app name not to be dependent on absolute import

--- studio_YAIVERSE/apps/main/apps.py
+++ studio_YAIVERSE/apps/main/apps.py
@@ -4,7 +4,7 @@ from django.apps import AppConfig
 class MainConfig(AppConfig):
     # Default Configs
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'studio_YAIVERSE.apps.main'
+    name = __name__.rsplit('.', 1)[0]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
