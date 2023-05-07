caaa5366386d9c12ed874ee5370bf91123a1b02b
kdha0727 <dhakim@yonsei.ac.kr>
2023-05-07 15:39:47 +0900

    patch2

--- studio_YAIVERSE/apps/accounts/views.py
+++ studio_YAIVERSE/apps/accounts/views.py
@@ -10,10 +10,6 @@ class UserRegisterView(mixins.CreateModelMixin, GenericViewSet):
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        if User.objects.get(serializer.validated_data['username']):
-            headers = self.get_success_headers(serializer.data)
-            return Response(serializer.data, status=status.HTTP_200_OK, headers=headers)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        if User.objects.filter(username=serializer.initial_data['username']).exists():
+            return Response({'username': request.data['username']}, status=status.HTTP_200_OK)
+        return super().create(request, *args, **kwargs)
